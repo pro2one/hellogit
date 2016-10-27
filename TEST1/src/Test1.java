0e7cbf929323cb2a7c7f8fadb8a92be88ae9fdc9
T410SI\pro2one <masaya.taniguchi@gmail.com>
2016-10-27 16:02:23 +0900

    コメント削除、ソース変更

--- TEST1/src/Test1.java
+++ TEST1/src/Test1.java
@@ -2,8 +2,8 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Work!");
+		// 
+		System.out.println("Hello Work2!");
 	}
 
 }
